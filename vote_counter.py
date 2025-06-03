c81319b195e37b4779b2933cc2177130892ddc62
machippo <78477805+machippo@users.noreply.github.com>
2025-06-03 18:50:51 +0900

    Create vote_counter.py

---
+++ vote_counter.py
@@ -0,0 +1,14 @@
+import requests
+def get_tupyo():
+    resp = requests.get("https://news.kbs.co.kr/vote/20250603/KBS_SUNGER_DATA.json?d=20250603183720")
+    num = resp.json()[0]['DONGSAMUSO_TUSU']
+    return int(num)
+    
+
+last_tupyo = get_tupyo()
+while True:
+    tupyo = get_tupyo()
+    if tupyo > last_tupyo:
+        print(f'new, {tupyo} votes')
+        last_tupyo = tupyo
+    time.sleep(1)
